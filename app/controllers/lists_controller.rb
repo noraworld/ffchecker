f53c965d516cbe6f84958d3f8c810e961098792e
noraworld <noraworld@mail.com>
2017-05-19 01:34:21 +0900

    :sparkles: Create lists controller

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,26 @@
+class ListsController < ApplicationController
+  before_action :signed_in?
+
+  @@client = Twitter::REST::Client.new do |config|
+    config.consumer_key        = ENV['TWITTER_CONSUMER_KEY']
+    config.consumer_secret     = ENV['TWITTER_CONSUMER_SECRET']
+    config.access_token        = ENV['TWITTER_ACCESS_TOKEN']
+    config.access_token_secret = ENV['TWITTER_ACCESS_TOKEN_SECRET']
+  end
+
+  def index
+    @lists = @@client.lists()
+  end
+
+  def show
+    @members = @@client.list_members(params[:listname])
+  end
+
+  private
+  def signed_in?
+    unless current_user
+      redirect_to '/signin'
+    end
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -12,8 +12,19 @@ Rails.application.routes.draw do
   get    'followers'               => 'followers#index'
   get    'followers/page/:num'     => 'followers#index'
 
+  get    'lists'                   => 'lists#index'
+  get    'lists/:listname'         => 'lists#show'
+
   post   'follow/:username'        => 'actions#create'
   delete 'unfollow/:username'      => 'actions#destroy'
+
+  # post   'mute/:username'
+  # delete 'unmute/:username'
+
+  # post   'block/:username'
+  # delete 'unblock/:username'
+
+  # post   'lists/move/:username/:listname_src/to/:listname_dest'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
